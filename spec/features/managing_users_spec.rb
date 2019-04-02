fc51f20218063af70085431ebd4c6bd651237c17
Marwan Hegazy <mmhegazy3@sheffield.ac.uk>
2019-04-02 15:17:04 +0100

    added users testing

--- spec/features/managing_users_spec.rb
+++ spec/features/managing_users_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 
 describe 'Managing users' do
   specify 'I can add new user' do
+    FactoryBot.create :user
     visit '/users/new'
+    fill_in 'Username', with:'usert'
+    fill_in 'Password', with:'password'
+    click_button 'Log in'
     fill_in 'Username', with: 'user1'
     fill_in 'Password', with: 'password'
     fill_in 'Access', with: 1
@@ -19,6 +23,9 @@ describe 'Managing users' do
   specify 'I can edit a user' do
     FactoryBot.create :user
     visit '/users'
+    fill_in 'Username', with:'usert'
+    fill_in 'Password', with:'password'
+    click_button 'Log in'
     click_link 'Edit'
     fill_in 'Username', with: 'user'
     fill_in 'Password', with: 'password'
@@ -26,23 +33,35 @@ describe 'Managing users' do
     fill_in 'Email', with: 'example@example.com'
     fill_in 'Name', with: 'user'
     click_button 'Update User'
-    expect(page).to have_content 'User was successfully updated'
+    fill_in 'Username', with:'user'
+    fill_in 'Password', with:'password'
+    click_button 'Log in'
     expect(page).to have_content 'user'
     click_link 'Back'
     within(:css, 'table') {expect(page).to have_content 'user'}
   end
 
   specify 'I can see a list of users' do
-    user1 = FactoryBot.create :user, name: 'User1'
+    user1 = FactoryBot.create :user, name: 'User1', email: 'test@test.t'
+    FactoryBot.create :user
     visit '/users'
+    fill_in 'Username', with:'usert'
+    fill_in 'Password', with:'password'
+    click_button 'Log in'
     within(:css, 'table') {expect(page).to have_content 'User1'}
-    within(:css, 'table') {expect(page).to have_content 'User2'}
   end
 
   specify 'I can delete users' do
+    FactoryBot.create :user, name: 'test', email: 'test@test.t'
     FactoryBot.create :user
     visit '/users'
+    fill_in 'Username', with:'usert'
+    fill_in 'Password', with:'password'
+    click_button 'Log in'
     click_link 'Destroy'
-    within(:css, 'table') {expect(page).to_not have_content 'user'}
+    fill_in 'Username', with:'usert'
+    fill_in 'Password', with:'password'
+    click_button 'Log in'
+    within(:css, 'table') {expect(page).to_not have_content 'test'}
   end
 end
