446bd33d5228ec63e330a3dd87b27f6e38d969ba
Collin Coutinho <ccoutinho1@sheffield.ac.uk>
2019-04-11 19:07:58 +0100

    Some seeds for the program, not finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,43 @@
 
 User.where(email:
 'user@a.a').first_or_create!(password:
-'password', access: 1, username: 'user', name: 'User')
+'password', access: 1, username: 'user',  institution: 'Sheffield University', name: 'User')
+
+User.where(email:
+'admin@a.a').first_or_create!(password:
+'password', access: 1, username: 'admin', institution: 'Sheffield University', name: 'Adminstrator')
+
+User.where(email:
+'delegate1@a.a').first_or_create!(password:
+'password', access: 2, username: 'delegate', institution: 'Sheffield University', name: 'Delegate 1')
+
+User.where(email:
+'delegate2@a.a').first_or_create!(password:
+'password', access: 2, username: 'delegate2', institution: 'Sheffield University', name: 'Delegate 2')
+
+User.where(email:
+'test@a.a').first_or_create!(password:
+'password', access: 2, username: 'test', institution: 'Sheffield University', name: 'Test User')
+
+Conference.create(days: 2, name: 'conference 1', location: 'Sheffield Hallam')
+
+
+Booking.create(booking_reference: 11111,institution: 'Sheffield University',
+ticket_type: 2, access_req: 'f', catering: 'f', attended: 'f',
+dietary_req: 'Nut allergy', user_id: 1, conference_id: 1)
+
+Booking.create(booking_reference: 111112,institution: 'Sheffield University',
+ticket_type: 2, access_req: 'f', catering: 'f', attended: 'f',
+dietary_req: 'Vegan', user_id: 2, conference_id: 1)
+
+Booking.create(booking_reference: 111113,institution: 'Sheffield University',
+ticket_type: 2, access_req: 'f', catering: 'f', attended: 'f',
+dietary_req: 'Vegetarian', user_id: 3, conference_id: 1)
+
+Booking.create(booking_reference: 111114,institution: 'Sheffield University',
+ticket_type: 2, access_req: 'f', catering: 'f', attended: 'f',
+dietary_req: 'Water allergy', user_id: 4, conference_id: 1)
+
+Booking.create(booking_reference: 111115,institution: 'Sheffield University',
+ticket_type: 2, access_req: 'f', catering: 'f', attended: 'f',
+dietary_req: 'Lactose Intolerant', user_id: 5, conference_id: 1)
