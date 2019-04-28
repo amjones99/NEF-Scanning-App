f5038ffb172978338033f286a7d38c84c1b2ca46
Adam Jones <ajones20@sheffield.ac.uk>
2019-04-29 00:58:10 +0100

    Error Corrected?

--- app/models/user.rb
+++ app/models/user.rb
@@ -87,7 +87,7 @@ class User < ApplicationRecord
     new_booking.attended = false
     new_booking.user_id = new_user.id
     # check Easter Week 2 for issue about conference ID pls.
-    new_booking.conference_id = 1
+    new_booking.conference_id = 2
     new_booking.save!
 
     UserMailer.welcome_email(new_user).deliver
