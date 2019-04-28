25d7563e26a245a4cde7b50522addb755af570fa
Adam Jones <ajones20@sheffield.ac.uk>
2019-04-28 22:14:43 +0100

    Added Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,22 +29,24 @@ User.where(email:
 Conference.create(days: 2, name: 'conference 1', location: 'Sheffield Hallam')
 
 
-Booking.create(booking_reference: 11111,institution: 'Sheffield University',
-ticket_type: 2, access_req: 'f', catering: 'f', attended: 'f',
+Booking.create(booking_reference: '111111111',institution: 'Sheffield University',
+ticket_type: 2, access_req: 'Wheelchair Access', catering: 'f', attended: 'f',
 dietary_req: 'Nut allergy', user_id: 1, conference_id: 1)
 
-Booking.create(booking_reference: 111112,institution: 'Sheffield University',
-ticket_type: 2, access_req: 'f', catering: 'f', attended: 'f',
+Booking.create(booking_reference: '111111112',institution: 'Sheffield University',
+ticket_type: 2, access_req: '', catering: 'f', attended: 'f',
 dietary_req: 'Vegan', user_id: 2, conference_id: 1)
 
-Booking.create(booking_reference: 111113,institution: 'Sheffield University',
-ticket_type: 2, access_req: 'f', catering: 'f', attended: 'f',
+Booking.create(booking_reference: '111111113',institution: 'Sheffield University',
+ticket_type: 2, access_req: '', catering: 'f', attended: 'f',
 dietary_req: 'Vegetarian', user_id: 3, conference_id: 1)
 
-Booking.create(booking_reference: 111114,institution: 'Sheffield University',
-ticket_type: 2, access_req: 'f', catering: 'f', attended: 'f',
+Booking.create(booking_reference: '111111114',institution: 'Sheffield University',
+ticket_type: 2, access_req: '', catering: 'f', attended: 'f',
 dietary_req: 'Water allergy', user_id: 4, conference_id: 1)
 
-Booking.create(booking_reference: 111115,institution: 'Sheffield University',
-ticket_type: 2, access_req: 'f', catering: 'f', attended: 'f',
+Booking.create(booking_reference: '111111115',institution: 'Sheffield University',
+ticket_type: 2, access_req: 'Braille Needed', catering: 'f', attended: 'f',
 dietary_req: 'Lactose Intolerant', user_id: 5, conference_id: 1)
+
+Notification.create(not_id: 'Test Notification', not_des: 'Hello World')
