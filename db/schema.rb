63c8fe887f43bb0db649e9680143d3ac326dd78b
Adam Jones <ajones20@sheffield.ac.uk>
2019-05-15 16:36:33 +0100

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_15_123100) do
+ActiveRecord::Schema.define(version: 2019_05_15_153103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,15 +53,6 @@ ActiveRecord::Schema.define(version: 2019_05_15_123100) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["priority", "run_at"], name: "delayed_jobs_priority"
-  end
-
-  create_table "events", force: :cascade do |t|
-    t.string "name"
-    t.string "speaker"
-    t.string "location"
-    t.string "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "notifications", force: :cascade do |t|
