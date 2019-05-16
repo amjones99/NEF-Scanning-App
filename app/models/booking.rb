2ba913e4d259c54fc6fbebf0a6b3b83394cdd20c
Marwan Hegazy <mmhegazy3@sheffield.ac.uk>
2019-05-16 14:45:15 +0100

    added booking

---
+++ app/models/booking.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: bookings
+#
+#  id                :bigint(8)        not null, primary key
+#  access_req        :text
+#  attended          :boolean
+#  booking_reference :string
+#  catering          :boolean
+#  certificate       :boolean          default(FALSE)
+#  dietary_req       :string
+#  institution       :string
+#  ticket_type       :integer
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  conference_id     :integer
+#  user_id           :integer
+#
+
+class Booking < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :conference
+
+  validates_presence_of :user, :ticket_type
+
+end
