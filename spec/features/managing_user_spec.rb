b96047f353fccf127689b167e174e9988511e3c9
Marwan Hegazy <mmhegazy3@sheffield.ac.uk>
2019-05-16 17:51:21 +0100

    testing

--- spec/features/managing_user_spec.rb
+++ spec/features/managing_user_spec.rb
@@ -80,4 +80,57 @@ describe 'Managing user' do
   #     click_button 'submit-password'
   #     expect(page).to have_content 'User was successfully updated'
   # end
+  specify 'I can not access the admin page' do
+    FactoryBot.create :user, access: 2
+    visit '/users/'
+    fill_in 'Username', with:'testinguser'
+    fill_in 'Password', with:'password'
+    click_button 'Log in'
+    visit '/bookings'
+    expect(page).to have_content 'Homepage'
+  end
+  specify 'I can not pick a chosen username' do
+    FactoryBot.create :user, access:2
+    FactoryBot.create :user, access:2, username: 'user', id: 2
+    visit '/users/account'
+    fill_in 'Username', with:'testinguser'
+    fill_in 'Password', with:'password'
+    click_button 'Log in'
+    click_link 'Edit Account'
+    fill_in 'Username', with: 'user'
+    #fill_in 'Password', with: 'password'
+    fill_in 'Email', with: 'example@example.com'
+    fill_in 'Name', with: 'tesingupdate'
+    click_button 'Update User'
+    expect(page).to have_content 'has already been taken'
+  end
+  specify 'I can not edit notifications' do
+    FactoryBot.create :user, access:2
+    visit '/'
+    fill_in 'Username', with:'testinguser'
+    fill_in 'Password', with:'password'
+    click_button 'Log in'
+    visit '/notifications'
+    expect(page).to_not  have_content 'Edit'
+  end
+  specify 'I can not access conference mangment page' do
+    FactoryBot.create :user, access:2
+    visit '/'
+    fill_in 'Username', with:'testinguser'
+    fill_in 'Password', with:'password'
+    click_button 'Log in'
+    visit '/conferences'
+    expect(page).to_not  have_content 'New Conferences'
+  end
+  specify 'I can not access booking mangment page' do
+    FactoryBot.create :user, access:2
+    visit '/'
+    fill_in 'Username', with:'testinguser'
+    fill_in 'Password', with:'password'
+    click_button 'Log in'
+    visit '/bookings'
+    expect(page).to_not  have_content 'New Booking'
+  end
+
+
 end
